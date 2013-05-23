03d87d113d7a015c0137663de01328d66d04866e
Jorge <jorge.aguirre.leon@gmail.com>
2013-05-23 03:06:37 +0300

    Update mkconfig.sh

--- config/mkconfig.sh
+++ config/mkconfig.sh
@@ -1,18 +1,3 @@
-#! /bin/sh
-
-cat > config.json <<EOF
-{
-    "username": "$DATABASE_USER",
-    "password": "$DATABASE_PASSWORD",
-    "database": "$DATABASE_NAME",
-    "host":     "$DATABASE_HOST",
-    "dialect":  "$DATABASE_DIALECT",
-    "port":     "$DATABASE_PORT",
-    "protocol": "$DATABASE_PROTOCOL",
-    "storage":  "$DATABASE_STORAGE",
-    "omitNull": true
-}
-EOF
 
 
 
