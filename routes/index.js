d6ba33dfe6c6064b595331fb670d9938e40384ee
Vaishali Mirchandani <vaishali.mirchandani@gmail.com>
2013-05-28 23:44:38 +0200

    Entrega 3.2 finalizada

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@
 /*
  * GET home page.
  */
+var count = require('.././count');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    res.render('index', {counter: count.getCount() });
 };
